915af345d6fe3f278ca03cac65bb7d9e7cbe8c7e
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2023-11-06 13:05:52 -0500

    added some more comments

---
+++ securethevault.js
@@ -0,0 +1,20 @@
+/*Create a variable that holds a string that says "You have received this message because you have been chosen to open an important vault.  Here is the secret combination."
+the combination is 10, 40, 39.
+Create a variable that gives an answer back of 10.  const num1 = 5 + 5
+Create a variable that gives an answer back of 40.  const num2 = 20 * 2
+Create a variable that gives and answer back of 39  const num3 = 239 % 100
+Create an alert that will print out the string and all three combination numbers in order along with spaces in between.
+*/
+
+//I named my variable message since it is a message and then gave it the value of exactly what my message should read.
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//I used const over let to create my arithmetic variables because a combination on a vault isn't going to change during this process
+const num1 = 5 + 5;
+//5 plus 5 = 10
+const num2 = 20 * 2;
+//10 times 2 = 40
+const num3 = 239 % 100;
+//239 divided by 100 leaves a remainder of 39
+//created an alert that would pop up and show the message that is in a string, along with the 3 combination answers
+alert(hey + " " + num1 + " " + num2 + " " + num3);
+//When putting this into the console it worked!
